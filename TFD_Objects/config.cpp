a47dabc400a32fb8aa6c2bc6b5b2b25898db9202
Heartbroken-Git <corentin196@gmail.com>
2016-05-25 16:46:05 +0200

    Mise en place initiale du TFD_Objects et de son contenu de base

---
+++ TFD_Objects/config.cpp
@@ -0,0 +1,10 @@
+class CfgPatches {
+	class TFD_Objects {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"A3_Structures_F_Mil_Flags"};
+		version = 1.0
+		author[] = {"Heartbroken"};
+	};
+};
