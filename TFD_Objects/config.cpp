8d44b04a263788d8d0afeed4263c1d32c7902246
Heartbroken-Git <corentin196@gmail.com>
2016-05-25 17:11:45 +0200

    Premier essai de drapeau avec celui de l'UE

--- TFD_Objects/config.cpp
+++ TFD_Objects/config.cpp
@@ -8,3 +8,19 @@ class CfgPatches {
 		author[] = {"Heartbroken"};
 	};
 };
+
+class CfgVehicles {
+	
+	class FlagCarrier;
+	
+	class TFD_DrapeauUE : FlagCarrier {
+		_generalMacro = "FlagCarrier"; 
+        scope = 2;
+		scopeCurator=2;		
+        displayName="Drapeau (UE)";
+		author = "Heartbroken";           
+		class EventHandlers {
+			init="(_this select 0) setFlagTexture '\TFD_Objects\Data\Drapeau_UE.paa'"
+		}
+	}
+}
