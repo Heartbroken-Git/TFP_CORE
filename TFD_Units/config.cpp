b69f2d6f88bd4c27bb2fcfca965b313eda3682c3
Heartbroken-Git <corentin196@gmail.com>
2016-06-05 12:22:05 +0200

    ajout du pompier de l'Air en polo

--- TFD_Units/config.cpp
+++ TFD_Units/config.cpp
@@ -171,6 +171,23 @@ class CfgWeapons {
 		};
 	};
 	
+	class TFD_PoloSP_air : U_Rangemaster {
+		_generalMacro = "U_Rangemaster"; 
+        scope = 2;
+		scopeCurator = 2;
+		scopeArsenal = 2;             
+        displayName="Polo (Pompier de l'Air)";
+		author = "Heartbroken";
+		hiddenSelections[]= {"camo"}; 
+		hiddenSelectionsTextures[]= {"\TFD_Units\Data\Uniform\PoloSP_air.paa"};
+		class ItemInfo: UniformItem{
+            uniformModel = "-";
+            uniformClass = TFD_PompierAir_polo;
+            containerClass = Supply40;
+            mass = 40;
+		};
+	};
+	
 	// WETSUIT
 	class U_B_Wetsuit;
 	class TFD_WetsuitHV : U_B_Wetsuit {
@@ -273,6 +290,28 @@ class CfgVehicles {
         respawnMagazines[] = {Chemlight_green, Chemlight_green};        // What ammunition the character respawns with.
         linkedItems[] = {TFD_CasquetteSP, ItemMap, ItemCompass, ItemWatch, ItemRadio};               // Which items the character has.
         respawnLinkedItems[] = {TFD_CasquetteSP, ItemMap, ItemCompass, ItemWatch, ItemRadio};        // Which items the character respawns with.
+    };
+	
+	class TFD_PompierAir_polo: B_RangeMaster_F       // Define of a new class, which parameters are inherited from B_Soldier_base_F, with exception of those defined below.
+    {
+        author = "Heartbroken";         // The name of the author of the asset, which is displayed in the editor.
+		displayName = "Pompier de l'Air (Polo)";
+		side = 1;
+		faction = "TFD_AA";
+        scope = 2;                          // 2 = class is available in the editor; 1 = class is unavailable in the editor, but can be accessed via a macro; 0 = class is unavailable (and used for inheritance only).
+        scopeCurator = 2;                   // 2 = class is available in Zeus; 0 = class is unavailable in Zeus.
+        scopeArsenal = 2;                   // 2 = class is available in the Virtual Arsenal; 0 = class is unavailable in the Virtual Arsenal.
+        uniformClass = "TFD_PoloSP_air";                          // This links this soldier to a particular uniform. For the details, see below.
+		hiddenSelections[] = {"Camo"};                             // List of model selections which can be changed with hiddenSelectionTextures[] and hiddenSelectionMaterials[] properties. If empty, model textures are used.
+        hiddenSelectionsTextures[] = {"\TFD_Units\Data\Uniform\PoloSP_air.paa"}; 
+        weapons[] = {Throw, Put};               // Which weapons the character has.
+        respawnWeapons[] = {Throw, Put};        // Which weapons the character respawns with.
+        Items[] = {FirstAidKit, FirstAidKit, FirstAidKit, FirstAidKit};                // Which items the character has.
+        RespawnItems[] = {FirstAidKit, FirstAidKit, FirstAidKit, FirstAidKit};         // Which items the character respawns with.
+        magazines[] = {Chemlight_green, Chemlight_green};               // What ammunition the character has.
+        respawnMagazines[] = {Chemlight_green, Chemlight_green};        // What ammunition the character respawns with.
+        linkedItems[] = {ItemMap, ItemCompass, ItemWatch, ItemRadio};               // Which items the character has.
+        respawnLinkedItems[] = {ItemMap, ItemCompass, ItemWatch, ItemRadio};        // Which items the character respawns with.
     };
 	
 	class B_diver_F;
