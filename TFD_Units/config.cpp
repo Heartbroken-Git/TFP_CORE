79f67e00c295abd8bf03c238934bccead5e6c798
Heartbroken-Git <corentin196@gmail.com>
2016-06-08 20:13:57 +0200

    ajout des pseudos casques F2 SP

--- TFD_Units/config.cpp
+++ TFD_Units/config.cpp
@@ -149,6 +149,41 @@ class CfgWeapons {
 		hiddenSelectionsTextures[]= {"\TFD_Units\Data\Headgear\CasquetteCadet.paa"};  
 	};
 	
+	// CASQUE F2
+	class H_HelmetB_plain_blk;
+	class TFD_CasqueF2_HDR : H_HelmetB_plain_blk {
+		_generalMacro = "H_HelmetB_plain_blk"; 
+        scope = 2;
+		scopeCurator = 2;
+		scopeArsenal = 2;             
+        displayName="Casque F2 (Homme du Rang)";
+		author = "Heartbroken";
+		hiddenSelections[]= {"camo"}; 
+		hiddenSelectionsTextures[]= {"\TFD_Units\Data\Headgear\CasqueF2_HDR.paa"};  
+	};
+	
+	class TFD_CasqueF2_SO : H_HelmetB_plain_blk {
+		_generalMacro = "H_HelmetB_plain_blk"; 
+        scope = 2;
+		scopeCurator = 2;
+		scopeArsenal = 2;             
+        displayName="Casque F2 (Sous-Officier)";
+		author = "Heartbroken";
+		hiddenSelections[]= {"camo"}; 
+		hiddenSelectionsTextures[]= {"\TFD_Units\Data\Headgear\CasqueF2_SO.paa"};  
+	};
+	
+	class TFD_CasqueF2_Off : H_HelmetB_plain_blk {
+		_generalMacro = "H_HelmetB_plain_blk"; 
+        scope = 2;
+		scopeCurator = 2;
+		scopeArsenal = 2;             
+        displayName="Casque F2 (Officier)";
+		author = "Heartbroken";
+		hiddenSelections[]= {"camo"}; 
+		hiddenSelectionsTextures[]= {"\TFD_Units\Data\Headgear\CasqueF2_Off.paa"};  
+	};
+	
 	// VESTES
 	
 	// RECYCLEUR
