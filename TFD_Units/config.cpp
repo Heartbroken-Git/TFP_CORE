9eb314f1d803305a5b6a7ceb4bdaeda46be9f626
Heartbroken-Git <corentin196@gmail.com>
2016-08-25 10:42:15 +0200

    ajout du personnel du SAMU

--- TFD_Units/config.cpp
+++ TFD_Units/config.cpp
@@ -574,6 +574,24 @@ class CfgWeapons {
             mass = 30;
 		};
 	};
+	
+	class U_C_Scientist;
+	class TFD_CombiSAMU : U_C_Scientist {
+		_generalMacro = "U_C_Scientist"; 
+        scope = 2;
+		scopeCurator = 2;
+		scopeArsenal = 2;             
+        displayName="Combinaison d'ambulancier (SAMU)";
+		author = "Heartbroken";
+		hiddenSelections[]= {"camo"}; 
+		hiddenSelectionsTextures[]= {"\TFD_Units\Data\Uniform\Combi_SAMU.paa"};
+		class ItemInfo: UniformItem{
+            uniformModel = "-";
+            uniformClass = TFD_Ambulancier_SAMU;
+            containerClass = Supply30;
+            mass = 30;
+		};
+	};
 };
 
 class CfgVehicles {
@@ -888,4 +906,32 @@ class CfgVehicles {
 				init = "(_this select 0) execVM ""\TFD_Units\Script\stopRandom.sqf"""
 			};
     };
+	
+	class C_scientist_F;                     // For inheritance to work, the base class has to be defined.
+    class TFD_Ambulancier_SAMU: C_scientist_F       // Define of a new class, which parameters are inherited from B_Soldier_base_F, with exception of those defined below.
+    {
+        author = "Heartbroken";         // The name of the author of the asset, which is displayed in the editor.
+		displayName = "Ambulancier";
+		side = 3;
+		faction = "CIV_F";
+        scope = 2;                          // 2 = class is available in the editor; 1 = class is unavailable in the editor, but can be accessed via a macro; 0 = class is unavailable (and used for inheritance only).
+        scopeCurator = 2;                   // 2 = class is available in Zeus; 0 = class is unavailable in Zeus.
+        scopeArsenal = 2;                   // 2 = class is available in the Virtual Arsenal; 0 = class is unavailable in the Virtual Arsenal.
+        uniformClass = "TFD_CombiSAMU";                          // This links this soldier to a particular uniform. For the details, see below.
+		hiddenSelections[] = {"Camo"};                             // List of model selections which can be changed with hiddenSelectionTextures[] and hiddenSelectionMaterials[] properties. If empty, model textures are used.
+        hiddenSelectionsTextures[] = {"\TFD_Units\Data\Uniform\Combi_SAMU.paa"}; 
+        weapons[] = {Throw, Put};               // Which weapons the character has.
+        respawnWeapons[] = {Throw, Put};        // Which weapons the character respawns with.
+        Items[] = {FirstAidKit, FirstAidKit, FirstAidKit, FirstAidKit};                // Which items the character has.
+        RespawnItems[] = {FirstAidKit, FirstAidKit, FirstAidKit, FirstAidKit};         // Which items the character respawns with.
+        magazines[] = {Chemlight_green, Chemlight_green, Chemlight_red, Chemlight_red};               // What ammunition the character has.
+        respawnMagazines[] = {Chemlight_green, Chemlight_green, Chemlight_red, Chemlight_red};        // What ammunition the character respawns with.
+        linkedItems[] = {ItemMap, ItemCompass, ItemWatch, ItemRadio};               // Which items the character has.
+        respawnLinkedItems[] = {ItemMap, ItemCompass, ItemWatch, ItemRadio};        // Which items the character respawns with.
+		class EventHandlers 
+			{
+				init = "(_this select 0) execVM ""\TFD_Units\Script\stopRandom.sqf"""
+			};
+    };
+	
 };
