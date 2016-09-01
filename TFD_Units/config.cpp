7a83de9d6c50b8f9bbb4afac5f4e9270141c55e4
Heartbroken-Git <corentin196@gmail.com>
2016-09-01 19:08:54 +0200

    ajout des gilets d'assaut lourd

--- TFD_Units/config.cpp
+++ TFD_Units/config.cpp
@@ -427,6 +427,41 @@ class CfgWeapons {
 		}
 	};
 	
+	//GILET D'ASSAUT LOURD
+	class V_PlateCarrierIAGL_oli;
+	class TFD_GiletAssautLourd_GIPN : V_PlateCarrierIAGL_oli {
+		_generalMacro = "V_PlateCarrierIAGL_oli"; 
+        scope = 2;
+		scopeCurator = 2;
+		scopeArsenal = 2;             
+        displayName="Gilet d'assaut lourd (GIPN)";
+		author = "Heartbroken";
+		hiddenSelections[]= {"camo1","camo2"}; 
+		hiddenSelectionsTextures[]= {"\TFD_Units\Data\Vest\GiletAssaut_GIPN.paa","\TFD_Units\Data\Vest\GiletAssautLourd_Noir.paa"};
+	};
+	
+	class TFD_GiletAssautLourd_Douane : V_PlateCarrierIAGL_oli {
+		_generalMacro = "V_PlateCarrierIAGL_oli"; 
+        scope = 2;
+		scopeCurator = 2;
+		scopeArsenal = 2;             
+        displayName="Gilet d'assaut lourd (Douane)";
+		author = "Heartbroken";
+		hiddenSelections[]= {"camo1","camo2"}; 
+		hiddenSelectionsTextures[]= {"\TFD_Units\Data\Vest\GiletAssaut_Douane.paa","\TFD_Units\Data\Vest\GiletAssautLourd_Noir.paa"};
+	};
+	
+	class TFD_GiletAssautLourd_GN : V_PlateCarrierIAGL_oli {
+		_generalMacro = "V_PlateCarrierIAGL_oli"; 
+        scope = 2;
+		scopeCurator = 2;
+		scopeArsenal = 2;             
+        displayName="Gilet d'assaut lourd (Gendarmerie)";
+		author = "Heartbroken";
+		hiddenSelections[]= {"camo1","camo2"}; 
+		hiddenSelectionsTextures[]= {"\TFD_Units\Data\Vest\GiletAssaut_GN.paa","\TFD_Units\Data\Vest\GiletAssautLourd_Noir.paa"};
+	};
+	
 	// UNIFORME
 	class UniformItem;
 	
