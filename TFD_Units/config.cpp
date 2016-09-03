e8dd42f0e520cf02fed221e755b45c148e0fcf59
Heartbroken-Git <corentin196@gmail.com>
2016-09-03 11:55:55 +0200

    ajout des casques de pilote

--- TFD_Units/config.cpp
+++ TFD_Units/config.cpp
@@ -217,6 +217,30 @@ class CfgWeapons {
 		hiddenSelectionsTextures[]= {"\TFD_Units\Data\Headgear\CasqueF2_PC.paa"};  
 	};
 	
+	//CASQUE HELICOPTERE
+	class H_PilotHelmetHeli_B;
+	class TFD_CasqueHeli_GN : H_PilotHelmetHeli_B {
+		_generalMacro = "H_PilotHelmetHeli_B"; 
+        scope = 2;
+		scopeCurator = 2;
+		scopeArsenal = 2;             
+        displayName="Casque de pilote d'helicoptere (Gendarmerie)";
+		author = "Heartbroken";
+		hiddenSelections[]= {"camo"}; 
+		hiddenSelectionsTextures[]= {"\TFD_Units\Data\Headgear\CasqueHeli_GN.paa"};  
+	};
+	
+	class TFD_CasqueHeli_SC : H_PilotHelmetHeli_B {
+		_generalMacro = "H_PilotHelmetHeli_B"; 
+        scope = 2;
+		scopeCurator = 2;
+		scopeArsenal = 2;             
+        displayName="Casque de pilote d'helicoptere (Securite Civile)";
+		author = "Heartbroken";
+		hiddenSelections[]= {"camo"}; 
+		hiddenSelectionsTextures[]= {"\TFD_Units\Data\Headgear\CasqueHeli_SC.paa"};  
+	};
+	
 	// VESTES
 	
 	// RECYCLEUR
