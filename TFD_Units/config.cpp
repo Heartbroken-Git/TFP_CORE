68c7ca6df899d51af6bee8ea0939d8db868ae981
Heartbroken-Git <corentin196@gmail.com>
2016-09-07 11:54:44 +0200

    ajout des pilotes d'hélico de la Gendarmerie et de la Securite Civile

--- TFD_Units/config.cpp
+++ TFD_Units/config.cpp
@@ -814,6 +814,41 @@ class CfgWeapons {
 		};
 	};
 	
+	class U_B_HeliPilotCoveralls;
+	class TFD_CombiPilote_GN : U_B_HeliPilotCoveralls {
+		_generalMacro = "U_B_HeliPilotCoveralls"; 
+        scope = 2;
+		scopeCurator = 2;
+		scopeArsenal = 2;             
+        displayName="Combinaison de pilote d'helicoptere (Gendarmerie)";
+		author = "Heartbroken";
+		hiddenSelections[]= {"camo"}; 
+		hiddenSelectionsTextures[]= {"\TFD_Units\Data\Uniform\Combi_piloteGN.paa"};
+		class ItemInfo: UniformItem{
+            uniformModel = "-";
+            uniformClass = TFD_Pilote_GN;
+            containerClass = Supply30;
+            mass = 30;
+		};
+	};
+	
+	class TFD_CombiPilote_SC : U_B_HeliPilotCoveralls {
+		_generalMacro = "U_B_HeliPilotCoveralls"; 
+        scope = 2;
+		scopeCurator = 2;
+		scopeArsenal = 2;             
+        displayName="Combinaison de pilote d'helicoptere (Securite Civile)";
+		author = "Heartbroken";
+		hiddenSelections[]= {"camo"}; 
+		hiddenSelectionsTextures[]= {"\TFD_Units\Data\Uniform\Combi_piloteSC.paa"};
+		class ItemInfo: UniformItem{
+            uniformModel = "-";
+            uniformClass = TFD_Pilote_SC;
+            containerClass = Supply30;
+            mass = 30;
+		};
+	};
+	
 	//PULL
 	class U_BG_Guerrilla_6_1;
 	class TFD_PullCE_AT : U_BG_Guerrilla_6_1 {
@@ -1305,6 +1340,59 @@ class CfgVehicles {
 				init = "(_this select 0) execVM ""\TFD_Units\Script\stopRandom.sqf"""
 			};
     };
+	
+	class B_Helipilot_F;
+	class TFD_Pilote_GN: B_Helipilot_F       // Define of a new class, which parameters are inherited from B_Soldier_base_F, with exception of those defined below.
+    {
+        author = "Heartbroken";         // The name of the author of the asset, which is displayed in the editor.
+		displayName = "Pilote";
+		side = 1;
+		faction = "TFD_GN";
+        scope = 2;                          // 2 = class is available in the editor; 1 = class is unavailable in the editor, but can be accessed via a macro; 0 = class is unavailable (and used for inheritance only).
+        scopeCurator = 2;                   // 2 = class is available in Zeus; 0 = class is unavailable in Zeus.
+        scopeArsenal = 2;                   // 2 = class is available in the Virtual Arsenal; 0 = class is unavailable in the Virtual Arsenal.
+        uniformClass = "TFD_CombiPilote_GN";                          // This links this soldier to a particular uniform. For the details, see below.
+		hiddenSelections[] = {"Camo"};                             // List of model selections which can be changed with hiddenSelectionTextures[] and hiddenSelectionMaterials[] properties. If empty, model textures are used.
+        hiddenSelectionsTextures[] = {"\TFD_Units\Data\Uniform\Combi_piloteGN.paa"}; 
+        weapons[] = {hgun_Rook40_F, Throw, Put};               // Which weapons the character has.
+        respawnWeapons[] = {hgun_Rook40_F, Throw, Put};        // Which weapons the character respawns with.
+        Items[] = {FirstAidKit};                // Which items the character has.
+        RespawnItems[] = {FirstAidKit};         // Which items the character respawns with.
+        magazines[] = {mag_4(16Rnd_9x21_Mag), Chemlight_green, Chemlight_green};               // What ammunition the character has.
+        respawnMagazines[] = {mag_4(16Rnd_9x21_Mag), Chemlight_green, Chemlight_green};        // What ammunition the character respawns with.
+        linkedItems[] = {TFD_CasqueHeli_GN, V_Rangemaster_belt, ItemMap, ItemCompass, ItemWatch, ItemRadio};               // Which items the character has.
+        respawnLinkedItems[] = {TFD_CasqueHeli_GN, V_Rangemaster_belt, ItemMap, ItemCompass, ItemWatch, ItemRadio};        // Which items the character respawns with.
+		class EventHandlers 
+			{
+				init = "(_this select 0) execVM ""\TFD_Units\Script\stopRandom.sqf"""
+			};
+    };
+	
+	class TFD_Pilote_SC: C_man_w_worker_F       // Define of a new class, which parameters are inherited from B_Soldier_base_F, with exception of those defined below.
+    {
+        author = "Heartbroken";         // The name of the author of the asset, which is displayed in the editor.
+		displayName = "Pilote (Securite Civile)";
+		side = 3;
+		faction = "CIV_F";
+        scope = 2;                          // 2 = class is available in the editor; 1 = class is unavailable in the editor, but can be accessed via a macro; 0 = class is unavailable (and used for inheritance only).
+        scopeCurator = 2;                   // 2 = class is available in Zeus; 0 = class is unavailable in Zeus.
+        scopeArsenal = 2;                   // 2 = class is available in the Virtual Arsenal; 0 = class is unavailable in the Virtual Arsenal.
+        uniformClass = "TFD_CombiPilote_SC";                          // This links this soldier to a particular uniform. For the details, see below.
+		hiddenSelections[] = {"Camo"};                             // List of model selections which can be changed with hiddenSelectionTextures[] and hiddenSelectionMaterials[] properties. If empty, model textures are used.
+        hiddenSelectionsTextures[] = {"\TFD_Units\Data\Uniform\Combi_piloteSC.paa"}; 
+        weapons[] = {Throw, Put};               // Which weapons the character has.
+        respawnWeapons[] = {Throw, Put};        // Which weapons the character respawns with.
+        Items[] = {FirstAidKit};                // Which items the character has.
+        RespawnItems[] = {FirstAidKit};         // Which items the character respawns with.
+        magazines[] = {Chemlight_green, Chemlight_green, Chemlight_red, Chemlight_red};               // What ammunition the character has.
+        respawnMagazines[] = {Chemlight_green, Chemlight_green, Chemlight_red, Chemlight_red};        // What ammunition the character respawns with.
+        linkedItems[] = {TFD_CasqueHeli_SC, ItemMap, ItemCompass, ItemWatch, ItemRadio};               // Which items the character has.
+        respawnLinkedItems[] = {TFD_CasqueHeli_SC, ItemMap, ItemCompass, ItemWatch, ItemRadio};        // Which items the character respawns with.
+		class EventHandlers 
+			{
+				init = "(_this select 0) execVM ""\TFD_Units\Script\stopRandom.sqf"""
+			};
+	};
 	
 	class B_G_Soldier_LAT_F;                     // For inheritance to work, the base class has to be defined.
     class TFD_Soldat_Pull: B_G_Soldier_LAT_F       // Define of a new class, which parameters are inherited from B_Soldier_base_F, with exception of those defined below.
