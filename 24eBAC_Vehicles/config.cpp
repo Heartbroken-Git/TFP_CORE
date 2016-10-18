779a70bb208d1b1d9beea8d7a971bddbcbb2b6a4
Heartbroken-Git <corentin196@gmail.com>
2016-10-18 23:39:55 +0200

    ajout du drone aux couleurs de l'Armée de l'Air

--- 24eBAC_Vehicles/config.cpp
+++ 24eBAC_Vehicles/config.cpp
@@ -1095,4 +1095,35 @@ class cfgVehicles
 			hiddenSelectionsTextures[]= {"\24eBAC_Vehicles\Data\EBG_DA\EBGDA_Corps.paa", "\24eBAC_Vehicles\Data\EBG_DA\EBGDA_Jupes.paa", "\24eBAC_Vehicles\Data\PVP_DA\PVPDA_Tourelle.paa","\24eBAC_Vehicles\Data\EBG_DA\EBGDA_Dozer.paa"};            
 			typicalCargo[] = {"B_engineer_F"}; 
 		};
+		
+	//UAV
+	class B_UAV_02_F;
+	class 24eBAC_UAV_AA:B_UAV_02_F
+        { 
+            _generalMacro = "B_UAV_02_F"; 
+            scope = 2; 
+            side = 1; 
+            faction = "TFD_AA";             
+            displayName="Drone MALE arme";
+			author = "Heartbroken";
+			crew = "B_UAV_AI";
+			hiddenSelections[]= {"camo1"};
+			hiddenSelectionsTextures[]= {"\24eBAC_Vehicles\Data\UAV_AA\UAV_AA.paa"};            
+			typicalCargo[] = {"B_UAV_AI"}; 
+		};
+		
+	class B_UAV_02_CAS_F;
+	class 24eBAC_UAVcas_AA:B_UAV_02_CAS_F
+        { 
+            _generalMacro = "B_UAV_02_CAS_F"; 
+            scope = 2; 
+            side = 1; 
+            faction = "TFD_AA";             
+            displayName="Drone MALE arme (CAS)";
+			author = "Heartbroken";
+			crew = "B_UAV_AI";
+			hiddenSelections[]= {"camo1"};
+			hiddenSelectionsTextures[]= {"\24eBAC_Vehicles\Data\UAV_AA\UAV_AA.paa"};            
+			typicalCargo[] = {"B_UAV_AI"}; 
+		};
 };
