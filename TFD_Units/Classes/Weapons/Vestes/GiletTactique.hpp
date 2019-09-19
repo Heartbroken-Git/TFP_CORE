05ff453c9e02421ad2a02b5c088cf19b86cd4fbe
Heartbroken-Git <corentin196@gmail.com>
2019-09-19 17:30:35 +0200

    ajout gilet Moyens Aériens de la PN

--- TFD_Units/Classes/Weapons/Vestes/GiletTactique.hpp
+++ TFD_Units/Classes/Weapons/Vestes/GiletTactique.hpp
@@ -88,6 +88,17 @@ class TFD_GiletTac_SC : V_TacVest_blk {
 	hiddenSelectionsTextures[]= {"\TFD_Units\Data\Vest\GiletTac_SC.paa"};
 };
 
+class TFD_GiletTac_PNma : V_TacVest_blk {
+	_generalMacro = "V_TacVest_blk";
+	scope = 2;
+	scopeCurator = 2;
+	scopeArsenal = 2;
+	displayName="Gilet pare-balles (Moyens Aeriens)";
+	author = "Heartbroken";
+	hiddenSelections[]= {"camo"};
+	hiddenSelectionsTextures[]= {"\TFD_Units\Data\Vest\GiletTac_PNma.paa"};
+};
+
 class V_TacVest_camo;
 class TFD_GiletTac_CE : V_TacVest_camo {
 	_generalMacro = "V_TacVest_camo";
