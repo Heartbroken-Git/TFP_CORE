eea5af6ec65796bd11cb8e3582786bda99399d9f
Heartbroken-Git <corentin196@gmail.com>
2019-09-23 10:27:39 +0200

    ajout gilets du RAID

--- TFD_Units/Classes/Weapons/Vestes/GiletAssautLourd.hpp
+++ TFD_Units/Classes/Weapons/Vestes/GiletAssautLourd.hpp
@@ -32,3 +32,14 @@ class TFD_GiletAssautLourd_GN : V_PlateCarrierIAGL_oli {
 	hiddenSelections[]= {"camo1","camo2"};
 	hiddenSelectionsTextures[]= {"\TFD_Units\Data\Vest\GiletAssaut_GN.paa","\TFD_Units\Data\Vest\GiletAssautLourd_Noir.paa"};
 };
+
+class TFD_GiletAssautLourd_RAID : V_PlateCarrierIAGL_oli {
+	_generalMacro = "V_PlateCarrierIAGL_oli";
+	scope = 2;
+	scopeCurator = 2;
+	scopeArsenal = 2;
+	displayName="Gilet d'assaut lourd (RAID)";
+	author = "Heartbroken";
+	hiddenSelections[]= {"camo1","camo2"};
+	hiddenSelectionsTextures[]= {"\TFD_Units\Data\Vest\GiletAssaut_RAID.paa","\TFD_Units\Data\Vest\GiletAssautLourd_Noir.paa"};
+};
