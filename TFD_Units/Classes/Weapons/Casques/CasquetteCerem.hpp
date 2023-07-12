163a23dc94ab031e43e0b5f196650ddb213c0876
Heartbroken-Git <corentin196@gmail.com>
2023-07-12 22:02:06 +0200

    première version casquette au grade de gendarme

---
+++ TFD_Units/Classes/Weapons/Casques/CasquetteCerem.hpp
@@ -0,0 +1,12 @@
+// CASQUETTE CEREMONIE
+class H_ParadeDressCap_01_base_F;
+class TFD_CasquetteCerem_GAIRgnd : H_ParadeDressCap_01_base_F {
+	_generalMacro = "H_ParadeDressCap_01_base_F";
+	scope = 2;
+	scopeCurator = 2;
+	scopeArsenal = 2;
+	displayName="Casquette (Gendarmerie de l'Air - Gendarme)";
+	author = "Heartbroken";
+	hiddenSelections[]= {"camo"};
+	hiddenSelectionsTextures[]= {"\TFD_Units\Data\Headgear\CasquetteCerem_GAIRgnd.paa"};
+};
