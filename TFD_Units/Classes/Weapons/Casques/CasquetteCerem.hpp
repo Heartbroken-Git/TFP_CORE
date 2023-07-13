6305dce501e8d844838c80d60832720548c5bfc3
Heartbroken-Git <corentin196@gmail.com>
2023-07-13 17:36:58 +0200

    ajout des casquettes des officiers de la GAIR

--- TFD_Units/Classes/Weapons/Casques/CasquetteCerem.hpp
+++ TFD_Units/Classes/Weapons/Casques/CasquetteCerem.hpp
@@ -65,3 +65,58 @@ class TFD_CasquetteCerem_GAIRmaj : H_ParadeDressCap_01_base_F {
 	hiddenSelections[]= {"camo"};
 	hiddenSelectionsTextures[]= {"\TFD_Units\Data\Headgear\CasquetteCerem_GAIRmaj.paa"};
 };
+
+class TFD_CasquetteCerem_GAIRltn : H_ParadeDressCap_01_base_F {
+	_generalMacro = "H_ParadeDressCap_01_base_F";
+	scope = 2;
+	scopeCurator = 2;
+	scopeArsenal = 2;
+	displayName="Casquette (Gendarmerie de l'Air - Lieutenant)";
+	author = "Heartbroken";
+	hiddenSelections[]= {"camo"};
+	hiddenSelectionsTextures[]= {"\TFD_Units\Data\Headgear\CasquetteCerem_GAIRltn.paa"};
+};
+
+class TFD_CasquetteCerem_GAIRcne : H_ParadeDressCap_01_base_F {
+	_generalMacro = "H_ParadeDressCap_01_base_F";
+	scope = 2;
+	scopeCurator = 2;
+	scopeArsenal = 2;
+	displayName="Casquette (Gendarmerie de l'Air - Capitaine)";
+	author = "Heartbroken";
+	hiddenSelections[]= {"camo"};
+	hiddenSelectionsTextures[]= {"\TFD_Units\Data\Headgear\CasquetteCerem_GAIRcne.paa"};
+};
+
+class TFD_CasquetteCerem_GAIRcen : H_ParadeDressCap_01_base_F {
+	_generalMacro = "H_ParadeDressCap_01_base_F";
+	scope = 2;
+	scopeCurator = 2;
+	scopeArsenal = 2;
+	displayName="Casquette (Gendarmerie de l'Air - Chef d'escadron)";
+	author = "Heartbroken";
+	hiddenSelections[]= {"camo"};
+	hiddenSelectionsTextures[]= {"\TFD_Units\Data\Headgear\CasquetteCerem_GAIRcen.paa"};
+};
+
+class TFD_CasquetteCerem_GAIRlcl : H_ParadeDressCap_01_base_F {
+	_generalMacro = "H_ParadeDressCap_01_base_F";
+	scope = 2;
+	scopeCurator = 2;
+	scopeArsenal = 2;
+	displayName="Casquette (Gendarmerie de l'Air - Lieutenant-colonel)";
+	author = "Heartbroken";
+	hiddenSelections[]= {"camo"};
+	hiddenSelectionsTextures[]= {"\TFD_Units\Data\Headgear\CasquetteCerem_GAIRlcl.paa"};
+};
+
+class TFD_CasquetteCerem_GAIRcol : H_ParadeDressCap_01_base_F {
+	_generalMacro = "H_ParadeDressCap_01_base_F";
+	scope = 2;
+	scopeCurator = 2;
+	scopeArsenal = 2;
+	displayName="Casquette (Gendarmerie de l'Air - Colonel)";
+	author = "Heartbroken";
+	hiddenSelections[]= {"camo"};
+	hiddenSelectionsTextures[]= {"\TFD_Units\Data\Headgear\CasquetteCerem_GAIRcol.paa"};
+};
