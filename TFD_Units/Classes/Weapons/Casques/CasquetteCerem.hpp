790342ed74dc7bb5d0191bd8501236b6155a2153
Heartbroken-Git <corentin196@gmail.com>
2023-07-13 13:12:37 +0200

    ajout version GAV

--- TFD_Units/Classes/Weapons/Casques/CasquetteCerem.hpp
+++ TFD_Units/Classes/Weapons/Casques/CasquetteCerem.hpp
@@ -1,5 +1,16 @@
 // CASQUETTE CEREMONIE
 class H_ParadeDressCap_01_base_F;
+class TFD_CasquetteCerem_GAIRgav : H_ParadeDressCap_01_base_F {
+	_generalMacro = "H_ParadeDressCap_01_base_F";
+	scope = 2;
+	scopeCurator = 2;
+	scopeArsenal = 2;
+	displayName="Casquette (Gendarmerie de l'Air - Volontaire)";
+	author = "Heartbroken";
+	hiddenSelections[]= {"camo"};
+	hiddenSelectionsTextures[]= {"\TFD_Units\Data\Headgear\CasquetteCerem_GAIRgav.paa"};
+};
+
 class TFD_CasquetteCerem_GAIRgnd : H_ParadeDressCap_01_base_F {
 	_generalMacro = "H_ParadeDressCap_01_base_F";
 	scope = 2;
